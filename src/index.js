75b65281db59663d05cbc3fd10c5ebb8394dc3fd
jeon-hakyung <jhakyung12@gmail.com>
2025-10-04 00:11:21 +0900

    부하 테스트 코드 추가

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import fs from "fs";
 import dotenv from "dotenv";
 import express from "express";       // -> ES Module
 import cors from "cors";
-import { checkDbConnection } from './db.config.js';
+import { checkDbConnection,pool } from './db.config.js';
 import { handleUserSignUp } from "./controllers/user.controller.js";
+import {body} from 'express-validator';
 
 
 dotenv.config();
+console.log("✅ index.js 실행됨");
+console.log("✅ PORT:", process.env.PORT);
 
 const app = express();
 const port = process.env.PORT;
@@ -20,8 +23,29 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post("/api/v1/users/signup",handleUserSignUp);
+app.post(
+  "/api/v1/users/signup",
+  [
+    body('email')
+      .notEmpty().withMessage('이메일은 필수 입력 항목입니다')
+      .isEmail().withMessage('유효한 이메일 형식이 아닙니다'),
+    body('password')
+      .notEmpty().withMessage('비밀번호는 필수 입력 항목입니다')
+      .isLength({min:8}).withMessage('비밀번호는 최소 8자 이상이어야 합니다.')
+  ], // ✅ 배열 뒤에 콤마(,) 추가
+  handleUserSignUp // ✅ 별도의 인자로 분리
+);
 
+// 테스트용 API
+app.get("/test", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT SLEEP(0.1)"); 
+    // 쿼리 실행 (0.1초 지연 -> 부하 테스트 체감 가능)
+    res.json({ message: "ok", result: rows });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 
 checkDbConnection().then(() => {
