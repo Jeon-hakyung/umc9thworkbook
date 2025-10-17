eccc11b50f4c49b270c1ef1c310caa5dec313480
jeon-hakyung <jhakyung12@gmail.com>
2025-10-17 23:23:52 +0900

    morgan 추가

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { checkDbConnection,pool } from './db.config.js';
 import { handleUserSignUp } from "./controllers/user.controller.js";
 import { handleAddStore,handleListStoreReviews } from "./controllers/store.controller.js";
 import {body} from 'express-validator';
+import morgan from "morgan";
+
 
 
 dotenv.config();
@@ -19,6 +21,7 @@ app.use(cors());                            // cors 방식 허용
 app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
